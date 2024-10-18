d89a00b3f4a037ace6bf9703bb2f290e06106728
aidtsang221 <aidanclaude.tsang@benilde.edu.ph>
2024-10-18 13:07:35 +0800

    Add Docker Files

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://mongo-db/TechExam");
   } catch (err) {
     console.error(err);
   }
